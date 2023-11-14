c665d95920532aca058a3d2f8f72694cdf9821c0
dknik <dknikolajk@gmail.com>
2023-11-14 15:32:54 +0100

    Added Canvas div and chart in JS

---
+++ main.js
@@ -0,0 +1,11 @@
+const ctx = document.querySelector('#chart').getContext('2d');
+const chart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ["2018","2019","2020","2021"],
+        datasets: [{
+            data: [1000, 820, 600, 460],
+            label: "Denmark"
+        }],
+    }
+})

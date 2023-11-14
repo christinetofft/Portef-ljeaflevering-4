aa5849e10658aff1d0a25f768f06eac2fed91cb3
christinetofft <christinetofft17@gmail.com>
2023-11-14 15:16:23 +0100

    Startup

---
+++ sweden-genre-sales.js
@@ -0,0 +1,37 @@
+const genreSales = [
+    {
+        "country" : "Sweden",
+        "name" : "Latin",
+        "TotalSales" : 31.5789
+    },
+    {
+        "country" : "Sweden",
+        "name" : "Rock",
+        "TotalSales" : 26.3158
+    },
+    {
+        "country" : "Sweden",
+        "name" : "Metal",
+        "TotalSales" : 18.4211
+    },
+    {
+        "country" : "Sweden",
+        "name" : "Alternative & Punk",
+        "TotalSales" : 15.7895
+    },
+    {
+        "country" : "Sweden",
+        "name" : "TV Shows",
+        "TotalSales" : 2.6316
+    },
+    {
+        "country" : "Sweden",
+        "name" : "Jazz",
+        "TotalSales" : 2.6316
+    },
+    {
+        "country" : "Sweden",
+        "name" : "World",
+        "TotalSales" : 2.6316
+    }
+]

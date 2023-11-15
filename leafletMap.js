23fea1f4a93dc1cb3415a84a32bb45e534489510
Kresky <kresky@protonmail.com>
2023-11-15 08:54:52 +0100

    Add leafletMap.js, and refer to it in index.html

---
+++ leafletMap.js
@@ -0,0 +1,5 @@
+const ufoMap = L.map('map').setView([56, 10.5], 7);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(ufoMap);

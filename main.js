aa9c251b06901720a03b7104a4ff8c86754cd0bc
christinetofft <christinetofft17@gmail.com>
2023-11-15 09:16:23 +0100

    Tilføjet javascript fil med data til Latin sales og tilføjet en horizontal bar i main.js

--- main.js
+++ main.js
@@ -11,3 +11,30 @@ const chart = new Chart(ctx, {
         }],
     }
 })
+
+console.log(latinSalesByCountries)
+
+let country = [];
+let percentageOfSales = [];
+latinSalesByCountries.forEach(sale => {
+    country.push(sale.BillingCountry)
+    percentageOfSales.push(sale.SalesPercentage)
+})
+
+const barColors = ["red"]
+const latinSalesChart = document.querySelector('#sales-by-countries').getContext('2d');
+
+new Chart(latinSalesChart, {
+    type: "bar",
+    data: {
+        labels: country,
+        datasets: [{
+            backgroundColor: barColors,
+            data: percentageOfSales
+        }]
+    },
+    options: {
+        indexAxis: 'y',
+        responsive: true
+    },}
+);
